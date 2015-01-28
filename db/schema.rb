0019dddc621b5db69b55954c8c3b6bff7e515e36
yunomac <yunomac@gmail.com>
2015-01-28 17:20:06 +0900

    5.7 모델관계선언

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128075754) do
+ActiveRecord::Schema.define(version: 20150128081340) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string   "title"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20150128075754) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bulletin_id"
   end
+
+  add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id"
 
 end
